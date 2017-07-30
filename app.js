c16388c691d328cebf32b540c8edda733d6be8c9
Milad <miladmirkarimi3@gmail.com>
2017-07-30 23:25:08 +1200

    updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var router = require('./router.js');
 var http = require('http');
 var fs = require("fs");
-
+var port = process.env.PORT || 8000;
 var server = http.createServer(function (request, response) {
   	router.css(request, response);
     router.home(request,response);
   	router.user(request,response);
 });
 
-server.listen(8000);
+server.listen(port);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
